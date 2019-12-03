d8b08f10939bce844062f551eff07b0a4cbb4782
AgueroRamiro <58488987+AgueroRamiro@users.noreply.github.com>
2019-12-03 18:40:18 -0300

    ads
    asd

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Que onda perro");
+            Console.WriteLine("Que onda ");
             Console.ReadKey();
         }
     }
